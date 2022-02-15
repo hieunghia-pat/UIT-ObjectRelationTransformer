ee059d66f3f1e7faa6ffb85aa6a590f72be8dbdc
hieunghia-pat <19520178@gm.uit.edu.vn>
2022-02-15 16:29:36 +0700

    imported sys module for misc/utils.py

--- misc/utils.py
+++ misc/utils.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import collections
+import sys
 import torch
 import torch.nn as nn
 import numpy as np
